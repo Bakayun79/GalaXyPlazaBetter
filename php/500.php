51cb67c5527cd1f7c50dc64af40ca73985c90620
Haruki <75132962+Bakayun79@users.noreply.github.com>
2021-10-03 18:37:10 +0200

    Create 500.php

---
+++ php/500.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html class="os-win"><head>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+        <meta charset="utf-8">
+        <title>500 Internal Server Error</title>
+        <meta http-equiv="content-style-type" content="text/css">
+        <meta http-equiv="content-script-type" content="text/javascript">
+        <meta name="format-detection" content="telephone=no">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, viewport-fit=cover">
+		<meta name="theme-color" content="#9900ff">
+        <meta name="apple-mobile-web-app-title" content="Galaxy Plaza">
+        <meta name="description" content="Galaxy Plaza is a service that lets you communicate with other users from around the world.">
+        <meta name="keywords" content="Miiverse,clone,Galaxy,Plaza,GalaxyPlaza">
+        <meta property="og:locale" content="en_US">
+        <meta property="og:title" content="Admin - Server Status - Galaxy Plaza">
+        <meta property="og:type" content="article">
+        <meta property="og:description" content="Galaxy Plaza is a service that lets you communicate with other users from around the world.">
+        <meta property="og:site_name" content="Galaxy Plaza">
+        <meta name="twitter:card" content="summary">
+        <meta name="twitter:domain" content="localhost">
+        <link rel="shortcut icon" href="/assets/img/favicon.ico">
+        <link rel="apple-touch-icon" sizes="57x57" href="/assets/img/apple-touch-icon-57x57.png">
+        <link rel="apple-touch-icon" sizes="114x114" href="/assets/img/apple-touch-icon-114x114.png">
+        <link rel="apple-touch-icon" sizes="72x72" href="/assets/img/apple-touch-icon-72x72.png">
+        <link rel="apple-touch-icon" sizes="144x144" href="/assets/img/apple-touch-icon-144x144.png">
+		<meta name="apple-mobile-web-app-capable" content="yes">
+		<meta name="apple-mobile-web-app-status-bar-style" content="#9900ff">
+		<meta name="mobile-web-app-capable" content="yes">
+		<link rel="stylesheet" type="text/css" href="/assets/css/offdevice.css">
+		<script src="/assets/js/site_script.js"></script>
+    </head>
+    <body class="" data-token="RiFLrLAC802UYIf5zQ-Z3g">
+        <div id="wrapper">
+            <div id="sub-body">
+                <menu id="global-menu">
+                    <li id="global-menu-logo">
+                        <h1><a><img src="/assets/img/menu-logo.png" alt="Galaxy Plaza" width="165" height="30"></a></h1>
+                    </li>
+                    <li id="global-menu-list">
+					</li>
+					</menu>
+            </div>
+            <div id="main-body">	<div class="main-column">
+		<div class="post-list-outline">
+			<h2 class="label">500 Internal Server Error</h2>
+			<section id="post-content" class="post post-subtype-default">
+				<ul class="list list-content-with-icon-and-text arrow-list">
+					<p>Sorry The Server Database is Down.</p>
+				</ul>
+			</section>
+		</div>
+	</div>
+            </div>
+            <div id="footer">
+                <div id="footer-inner">
+                    <div class="link-container">
+                        <p><a href="http://bakayun-contact.rf.gd">Contact Us</a></p>
+                        <p id="copyright"><a href="https://nintendo.com/">Galaxy Plaza is a nonprofit fan project based on assets from Nintendo and Hatena, and is not affiliated with either company.</a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    
+</body></html>
